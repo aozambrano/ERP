ec7e9d9e1ed53e0eca0ff2fff2c9cc6e86f39937
aozambrano <andreszambrano18@gmail.com>
2018-12-20 15:33:03 -0500

    Creación proyecto BusinessLayer
    Creación proyecto BusinessLayer

---
+++ BusinessLayer/src/main/java/com/erp/Andgui/BusinessLayer/BusinessLayerApplication.java
@@ -0,0 +1,14 @@
+package com.erp.Andgui.BusinessLayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessLayerApplication.class, args);
+	}
+
+}
+
---
+++ BusinessLayer/src/test/java/com/erp/Andgui/BusinessLayer/BusinessLayerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.erp.Andgui.BusinessLayer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BusinessLayerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
